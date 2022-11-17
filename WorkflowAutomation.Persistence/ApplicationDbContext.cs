3be22651f1a456dcd28b8151c3205e27ec47fbf1
Maksim-Shk <83287111+Maksim-Shk@users.noreply.github.com>
2022-11-17 13:14:25 +0500

    Добавлен контекст пользователя

---
+++ WorkflowAutomation.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.Extensions.Options;
+using WorkflowAutomation.Application.Interfaces;
+using WorkflowAutomation.Domain;
+using WorkflowAutomation.Persistence.EntityTypeConfigurations;
+
+namespace WorkflowAutomation.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
