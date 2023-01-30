f35d50f86e43cf67db1fffa89ffdce71818a81be
Maksim-Shk <83287111+Maksim-Shk@users.noreply.github.com>
2023-01-30 21:30:19 +0500

    ShortUserInfo

---
+++ WorkflowAutomation.Application/Documents/Queries/GetDocumentList/ShortUserInfo.cs
@@ -0,0 +1,8 @@
+namespace WorkflowAutomation.Application.Documents.Queries.GetDocumentList
+{
+    public class ShortUserInfo
+    {
+        public string UserId { get; set; }
+        public string UserInfo { get; set; }
+    }
+}
