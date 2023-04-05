6b872f7d6aa79a66b415ab698ff7467d5f44feda
Maksim-Shk <83287111+Maksim-Shk@users.noreply.github.com>
2023-04-05 16:23:46 +0500

    Добавлены классы команды обновления подразделения

---
+++ WorkflowAutomation.Application/Subdivisions/Commands/UpdateSubdivision/UpdateSubdivisionCommand.cs
@@ -0,0 +1,34 @@
+using MediatR;
+
+namespace WorkflowAutomation.Application.Subdivisions.Commands.UpdateSubdivision
+{
+    public class UpdateSubdivisionCommand : IRequest<int>
+    {
+        /// <summary>
+        /// ID измененного подразделения
+        /// </summary>
+        public int? SubdivisionId { get; set; }
+        /// <summary>
+        /// ID пользователя, обновляющего данные о подразделении
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// Название подразделения
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Дата создания подразделения
+        /// </summary>
+        // задается автоматически при создании, но может быть изменена
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// ID подразделения, к которому будет перепривязано текущее подразделение
+        /// </summary>
+        public int? SubordinationId { get; set; }
+        /// <summary>
+        /// Список пользователей с изменениями
+        /// </summary>
+        public List<UpdatesSubUsers>? SubdivisionUsers { get; set; }
+
+    }
+}
